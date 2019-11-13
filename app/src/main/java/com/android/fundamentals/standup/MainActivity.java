bad12f2074dbc768b56f4dea1394f8f356882c97
iamAbayomi <Oladinifolawiyo@gmail.com>
2019-11-13 21:32:59 +0100

    Initial Commit

---
+++ app/src/main/java/com/android/fundamentals/standup/MainActivity.java
@@ -0,0 +1,153 @@
+package com.android.fundamentals.standup;
+
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
+
+/** Non-Optimal Solution of Android Media Player
+ *
+ * Main Activity to display Notifications
+  and set Alarm with Alarm Manager
+ **/
+
+public class MainActivity extends AppCompatActivity {
+    // Notification ID.
+    private static final int NOTIFICATION_ID = 0;
+    // Notification channel ID.
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
+    private NotificationManager mNotificationManager;
+
+    MediaPlayer mediaPlayer;
+
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Creating 
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.eminem);
+
+        mNotificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
+
+        // Set up the Notification Broadcast Intent.
+        Intent notifyIntent = new Intent(this, AlarmReceiver.class);
+
+        boolean alarmUp = (PendingIntent.getBroadcast(this, NOTIFICATION_ID,
+                notifyIntent, PendingIntent.FLAG_NO_CREATE) != null);
+        alarmToggle.setChecked(alarmUp);
+
+        final PendingIntent notifyPendingIntent = PendingIntent.getBroadcast
+                (this, NOTIFICATION_ID, notifyIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final AlarmManager alarmManager = (AlarmManager) getSystemService
+                (ALARM_SERVICE);
+
+        // Set the click listener for the toggle button.
+        alarmToggle.setOnCheckedChangeListener
+                (new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged
+                            (CompoundButton buttonView, boolean isChecked) {
+                        String toastMessage;
+                        if (isChecked) {
+
+                            long repeatInterval = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+
+                            long triggerTime = SystemClock.elapsedRealtime()
+                                    + repeatInterval;
+                            //Setting the calender
+                            Calendar calendar = Calendar.getInstance();
+                            calendar.setTimeInMillis(System.currentTimeMillis());
+                            calendar.set(Calendar.HOUR_OF_DAY, 9);
+                            calendar.set(Calendar.MINUTE, 20);
+
+                            // If the Toggle is turned on, set the repeating alarm with
+                            // a 15 minute interval.
+                            if (alarmManager != null) {
+                                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
+                                                calendar.getTimeInMillis(), 5*60*1000,
+                                                notifyPendingIntent);
+                                //var mediaPlayer: MediaPlayer? = MediaPlayer.create(applicationContext, R.raw.eminem)
+                               // mediaPlayer?.start()
+
+                                mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.eminem);
+                               mediaPlayer.start();
+                            }
+                            // Set the toast message for the "on" case.
+                            toastMessage = "Alarm  On";
+
+                           } else {
+                            // Cancel notification if the alarm is turned off.
+                            mNotificationManager.cancelAll();
+
+                            if (alarmManager != null) {
+                                alarmManager.cancel(notifyPendingIntent);
+                            }
+                            // Set the toast message for the "off" case.
+                            toastMessage = "Alarm Off";
+                            mediaPlayer.stop();
+
+                        }
+
+                        // Show a toast to say the alarm is turned on or off.
+                        Toast.makeText(MainActivity.this, toastMessage,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Create the notification channel.
+        createNotificationChannel();
+    }
+
+
+    /**
+     * Creates a Notification channel, for OREO and higher.
+     */
+    public void createNotificationChannel() {
+
+        // Create a notification manager object.
+        mNotificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+
+            // Create the NotificationChannel with all the parameters.
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Stand up notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notifies every 15 minutes to " +
+                    "stand up and walk");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+}
